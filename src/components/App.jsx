7d51f87a0936bf004ca1f43c4c713e13fdf4ef3b
Kushnierova <Katerynakushnierova@gmail.com>
2023-12-22 17:31:41 +0100

    17:31

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Searchbar from './Searchbar';
 
 class App extends Component {
+  state = {
+    searchText: '',
+  };
+
+  handleFormSubmit = searchText => {
+    this.setState({ searchText });
+  };
   render() {
     return (
       <div>
         <div>
-          <Searchbar />
+          <Searchbar onSubmit={this.handleFormSubmit} />
         </div>
+        <ToastContainer autoClose={3000} />
       </div>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 import css from './Searchbar.module.css';
+import { toast } from 'react-toastify';
 import { ImSearch } from 'react-icons/im';
 
 class Searchbar extends Component {
-  state = {};
+  state = {
+    searchText: '',
+  };
+  handleNameChange = e => {
+    this.setState({ searchText: e.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.searchText.trim() === '') {
+      toast.error('string is empty');
+      return;
+    }
+    this.props.onSubmit(this.state.searchText);
+    this.setState({ searchText: '' });
+  };
   render() {
     return (
-      <div className={css.searchbar}>
-        <form className={css.searchForm}>
+      <header className={css.searchbar}>
+        <form onSubmit={this.handleSubmit} className={css.searchForm}>
           <label htmlFor="">
-            <button className={css.btn}>
+            <button type="submit" className={css.btn}>
               <ImSearch className={css.btnLabel} />
             </button>
           </label>
 
-          <input className={css.input}/>
+          <input
+            type="text"
+            name="searchText"
+            value={this.searchText}
+            onChange={this.handleNameChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            className={css.input}
+          />
         </form>
-      </div>
+      </header>
     );
   }
 }
