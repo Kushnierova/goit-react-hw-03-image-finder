0419c3a20ffeea68bbc2372a90239840536719fa
Kushnierova <Katerynakushnierova@gmail.com>
2023-12-22 16:34:31 +0100

    16:34

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Searchbar from './Searchbar';
 
-class App extends Component  {
-render(){
-  return(
-    <>
-    </>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Searchbar />
+        </div>
+      </div>
+    );
+  }
 }
-};
 
 export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,1 +1,22 @@
-Searchbar
+import React, { Component } from 'react';
+import css from './Searchbar.module.css'
+import { ImSearch } from 'react-icons/im';
+
+class Searchbar extends Component {
+  state = {};
+  render() {
+    return (
+      <div className={css.searchbar}>
+        <form className={css.searchForm}>
+            <label htmlFor="">
+                            <button className={css.btn}><ImSearch style={{ marginRight: 8 }} /></button>
+            </label>
+
+          <input />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
