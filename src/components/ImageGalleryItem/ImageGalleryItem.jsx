d317060d3041147c0d0287b00511a4cc83c7a760
Kushnierova <Katerynakushnierova@gmail.com>
2023-12-24 14:59:38 +0100

    14:59

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,16 +1,18 @@
 import css from './ImageGalleryItem.module.css';
 
-function ImageGalleryItem = pictures => {
-return (  <ul className={css.imageGallery}>
-    {pictures.map(picture => (
-      <li className={css.item} key={picture.id}>
-        <img
-          src={picture.webformatURL}
-          alt={picture.tags}
-          className={css.img}
-        />
-      </li>
-    ))}
-  </ul>;)
-};
+function ImageGalleryItem({ pictures }) {
+  return (
+    <ul className={css.imageGallery}>
+      {pictures.map(picture => (
+        <li className={css.item} key={picture.id}>
+          <img
+            src={picture.webformatURL}
+            alt={picture.tags}
+            className={css.img}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
 export default ImageGalleryItem;
